81ebf2b9ae25257fac38d8d7e33d6c06bda55e4b
USAN <dogs1012@naver.com>
2016-09-30 19:29:50 +0900

    CREATE project

---
+++ gitproject/src/com/test/B01.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class B01 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("HELLO");
+
+	}
+
+}
